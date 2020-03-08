f7c39ae3aaabb60ace51d94243919f9dfc017610
paukey <paukey@163.com>
2020-03-08 14:40:55 +0800

    chapter2 example

---
+++ chapter02/2.4_nogood.c
@@ -0,0 +1,17 @@
+/*nogood.c--有错误的程序*/
+/*并进行改正*/
+
+#include <stdio.h>
+
+int main(void){
+int n,n2,n3;
+n=5;
+n2=n*n;
+n3=n*n2;
+
+printf("n=%d,\nn squared=%d,\nn cubed=%d\n",n,n2,n3);
+
+return 0;
+
+
+}
